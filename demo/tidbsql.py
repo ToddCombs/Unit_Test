15f48a257b75f0b86fd5bf6715505360a4cead27
admin <fraulen.shen@gmail.com>
2022-04-02 11:53:05 +0800

    隐藏ip

--- demo/tidbsql.py
+++ demo/tidbsql.py
@@ -5,7 +5,7 @@ import pymysql
 # from demo.Locust_demo.locust_tidb import TidbTaskSet
 
 db = pymysql.connect(
-    host='tidb.uat.etcp.net', port=5000, user='parking_dev', password='fTg1DIUKb81#tP3#', db='parking')
+    host='XXXX', port=5000, user='XXXX', password='XXXX', db='XXXX')
 cursor = db.cursor()
 sql = "SELECT c.`platenumber` FROM caroutpayment c LEFT JOIN parking_record p ON c.`platenumber` = p.`plate_number` WHERE c.`parkingid` = 6089 AND p.`area_id` AND p.`state` = 0 AND p.`status` < 3 GROUP BY c.`platenumber`;"                  # % (random.choice(TidbTaskSet.sql_parkingid), random.choice(TidbTaskSet.sql_state))
 ret = time.time()
