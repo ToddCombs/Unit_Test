d152a289f429f9f9a610bfdb9d4f57d16a16377a
admin <fraulen.shen@gmail.com>
2022-06-15 13:56:00 +0800

    纯web服务。无业务

--- demo/Locust_demo/pushBilling_status.py
+++ demo/Locust_demo/pushBilling_status.py
@@ -4,4 +4,22 @@ from icecream import ic
 from locust import task, TaskSet
 from locust.contrib.fasthttp import FastHttpUser
 
-class web_status
+
+class Web_Status(TaskSet):
+    """纯Web服务，无业务"""
+
+    @task(10)
+    def Push_Privilege_Status(self):
+        res = self.client.get("/privilege/status")
+        if res.status_code != 200:
+            ic("出错了，错误信息：", res.text)
+        else:
+            # ic(res.text)
+            pass
+
+
+class Web_User(FastHttpUser):
+    tasks = [Web_Status]
+    min_wait = 1000
+    max_wait = 3000
+    host = "http://10.103.22.88:8090"  # qa
