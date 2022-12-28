45d07e81d9d30d5533f4f20f9dd1f0a1a7f9cfc4
admin <fraulen.shen@gmail.com>
2022-12-28 08:53:14 +0800

    report2022

--- demo/Locust_demo/locust_report2022.py
+++ demo/Locust_demo/locust_report2022.py
@@ -1,4 +1,4 @@
-# 2022出行报告小程序压测
+# 2022出行报告小程序压测——shenyu
 import random
 import numpy as np
 import pandas as pd
@@ -12,7 +12,7 @@ class Reporter(TaskSet):
     :param:userId
     :return:res.text
     """
-    # data = np.array(pd.read_csv())
+    data = np.array(pd.read_csv("demo/data/userid10000.csv"))
 
     def on_start(self):
         ic("------ 开始压测 ------")
@@ -20,25 +20,25 @@ class Reporter(TaskSet):
     def on_stop(self):
         ic("------ Test over ------")
 
-    # @task(1)
-    # def query_parking_details(self):
-    #     """2022年度账单接口1详细信息"""
-    #     res = self.client.get("hp/2022/1/?user_id=" + str(random.choice(Reporter.data)[0]), name='年度账单详情接口1')
-    #     if res.status_code != 200:
-    #         ic(res.text)
-    #     else:
-    #         pass
-    #
-    # # @task(1)
-    # def query_parking(self):
-    #     """2022年度账单接口2"""
-    #     res = self.client.get("hp/2022/2/?user_id=" + str(random.choice(Reporter.data)[0]), name='年度账单接口2')
-    #     if res.status_code != 200:
-    #         ic(res.text)
-    #     else:
-    #         pass
+    @task(4)
+    def query_parking_details(self):
+        """2022年度账单接口1详细信息"""
+        res = self.client.get("hp/2022/1/?user_id=" + str(random.choice(Reporter.data)[0]), name='年度账单详情接口1')
+        if res.status_code != 200:
+            ic(res.text)
+        else:
+            pass
 
-    @task(1)
+    @task(4)
+    def query_parking(self):
+        """2022年度账单接口2"""
+        res = self.client.get("hp/2022/2/?user_id=" + str(random.choice(Reporter.data)[0]), name='年度账单接口2')
+        if res.status_code != 200:
+            ic(res.text)
+        else:
+            pass
+
+    @task(2)
     def no_result(self):
         """查询无结果测试"""
         res = self.client.get("hp/2022/" + str(random.choice([1, 2])) + "/?user_id=no_result", name='无查询结果')
