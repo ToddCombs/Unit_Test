82cc1c80b0aac83e5fb98753407908509ecd1d1b
fraulen.shen@gmail.com <fraulen.shen@gmail.com>
2023-03-20 22:41:04 +0800

    exercise

--- demo/MoshApp.py
+++ demo/MoshApp.py
@@ -73,17 +73,17 @@
 # if has_good_credit and not has_criminal_record:
 #     print("Eligible for loan")
 
-# temperature = 30
-# if temperature > 30:
-#     print("It's a hot day")
-# else:
-#     print("It's not a hot day")
-#
-# name = "Todd Combs"
-# if len(name) < 3:
-#     print("Name must be at least 3 characters.")
-# elif len(name) > 50:
-#     print("Name must be a maximum of 50 characters.")
-# else:
-#     print("Name looks good!")
+temperature = 30
+if temperature > 30:
+    print("It's a hot day")
+else:
+    print("It's not a hot day")
+
+name = "Todd Combs"
+if len(name) < 3:
+    print("Name must be at least 3 characters.")
+elif len(name) > 50:
+    print("Name must be a maximum of 50 characters.")
+else:
+    print("Name looks good!")
 
