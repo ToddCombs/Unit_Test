8a1b8d34f5e095cf0da0792e6a93b94136b21422
fraulen.shen@gmail.com <fraulen.shen@gmail.com>
2023-03-20 22:39:43 +0800

    exercise

--- demo/MoshApp.py
+++ demo/MoshApp.py
@@ -58,10 +58,32 @@
 # else:
 #     print("It's a lovely day")
 # print("Enjoy your day")
-price = 1000000
-has_good_credit = True
-if has_good_credit:
-    down_payment = 0.1 * price
-else:
-    down_payment = 0.2 * price
-print(f"Down payment: ${down_payment}")
+# price = 1000000
+# has_good_credit = True
+# if has_good_credit:
+#     down_payment = 0.1 * price
+# else:
+#     down_payment = 0.2 * price
+# print(f"Down payment: ${down_payment}")
+#
+# has_high_income = False
+# has_good_credit = True
+# has_criminal_record = False
+# # if has_high_income or has_good_credit:
+# if has_good_credit and not has_criminal_record:
+#     print("Eligible for loan")
+
+# temperature = 30
+# if temperature > 30:
+#     print("It's a hot day")
+# else:
+#     print("It's not a hot day")
+#
+# name = "Todd Combs"
+# if len(name) < 3:
+#     print("Name must be at least 3 characters.")
+# elif len(name) > 50:
+#     print("Name must be a maximum of 50 characters.")
+# else:
+#     print("Name looks good!")
+
